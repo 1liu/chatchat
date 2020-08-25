4c2b9ffd18c8a79e7f4cac9d63a7cae865e4c202
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-08-25 13:29:40 -0700

    created socket join and sendMessage events

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 const router = require('./router');
 
 const app = express();
@@ -11,9 +12,29 @@ const io = socketio(server);
 io.on('connection', (socket) => {
   console.log('a user connected');
 
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error);
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` })
+    //emitting to all connected sockets except itself
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` })
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+    //emitting to all connected sockets
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  })
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+
 });
 
 app.use(router);
